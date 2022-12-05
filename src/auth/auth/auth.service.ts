366171dec0a4d3c3682ce9b75e177b183d836fd6
Thiago Pereira <thiagopereiramail@gmail.com>
2022-12-05 18:26:10 -0300

    Adicionar papeis ao usuários

--- src/auth/auth/auth.service.ts
+++ src/auth/auth/auth.service.ts
@@ -7,16 +7,19 @@ const users = [
     id: 1,
     username: 'user1@user.com',
     password: '$2b$10$.6vVP5Nr92EDSTGvrrbckOkHb2yrftwOgcBkKzLQNIgXoBYvDWSeq', //123
+    role: 'admin',
   },
   {
     id: 2,
     username: 'user2@user.com',
     password: '$2b$10$swbr4Y/k7q3ML2QvTFpCeOyOQEmNhKSpslHdg/wUtTce2NOL4bziK', //456
+    role: 'user',
   },
   {
     id: 3,
     username: 'user3@user.com',
     password: '$2b$10$UK.hSZk7GnJLMs0pyZm9Q.INWi0hO34VUeACCQGbBhoDprf7pzk/6', //789
+    role: 'user',
   },
 ];
 
@@ -31,6 +34,7 @@ export class AuthService {
       // subject: Dono do token, ou para quem ele foi gerado!
       sub: user.id,
       username: user.username,
+      role: user.role,
     };
 
     return this.jwtService.sign(payload);
